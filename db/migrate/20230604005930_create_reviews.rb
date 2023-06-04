8d7a2d2be96069366ea0a871134838d9504ed1a6
takashi-kinoshita <121014620+takashi-kinoshita@users.noreply.github.com>
2023-06-04 09:59:59 +0900

    feat: ✨ Reviewモデルの作成

---
+++ db/migrate/20230604005930_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :shop_id
+      t.text :content
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
