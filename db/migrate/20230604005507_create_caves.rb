fd28cb9f3081acf8618c94859c82daa927b790b7
takashi-kinoshita <121014620+takashi-kinoshita@users.noreply.github.com>
2023-06-04 09:57:09 +0900

    feat: ✨ Cafeモデルの作成

---
+++ db/migrate/20230604005507_create_caves.rb
@@ -0,0 +1,18 @@
+class CreateCaves < ActiveRecord::Migration[7.0]
+  def change
+    create_table :caves do |t|
+      t.string :name
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.string :phone
+      t.string :opening
+      t.string :closed
+      t.string :images
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
