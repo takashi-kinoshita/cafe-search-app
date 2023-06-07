0577efa2e05460b1c08adec0ca4a9fc5cffefa64
takashi-kinoshita <121014620+takashi-kinoshita@users.noreply.github.com>
2023-06-07 09:38:29 +0900

    feat: 🎨 WebpackerとBootstrapの設定追加

--- db/migrate/20230604005930_create_reviews.rb
+++ db/migrate/20230604005930_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.integer :user_id
-      t.integer :shop_id
+      t.integer :cafe_id
       t.text :content
       t.datetime :created_at
       t.datetime :updated_at
